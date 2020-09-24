9de67bea436810d764bbe76e693a87ab5d46a124
thutruong84 <71114216+thutruong84@users.noreply.github.com>
2020-09-23 19:54:12 -0700

    Add HelloWorld.java file

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+
+/*
+    To compile: javac HelloWorld.java
+    To run: java HelloWorld
+ */
+
+// declares a class hello world
+public class HelloWorld {
+
+    // "main" makes this class a java app that can be executed
+    public static void main(final String[] args) {
+
+        // output hello world to the system's output device
+        System.out.println("Hello world!");
+        
+    }
+
+}
+
